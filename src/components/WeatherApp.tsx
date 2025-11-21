0b7f9110bc2fcdd5c34e5da34e89685e5eed178b
Trang <nguyenminhtrang9213@gmail.com>
2025-11-21 21:50:26 +0100

    update

--- src/components/WeatherApp.tsx
+++ src/components/WeatherApp.tsx
@@ -15,13 +15,13 @@ function WeatherApp(): JSX.Element {
             <div className="location">Düsseldorf</div>
           </div>
 
-          <div className="search-bar flex justify-between items-center border-2 rounded-full">
+          <div className="search-bar flex justify-between items-center  border-purple-700  border-2 rounded-full">
             <input
               type="text"
               placeholder="Enter location"
               className="flex-1 text-xl outline-none px-4 py-2 placeholder:text-md"
             />
-            <Search className="cursor-pointer hover:scale-105 transition duration-150 mr-1" />
+            <Search className="cursor-pointer text-purple-700 hover:scale-105 transition duration-150 mr-1" />
           </div>
         </div>
         <div className="text-center">
