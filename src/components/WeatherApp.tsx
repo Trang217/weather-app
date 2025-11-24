27ee6b13631c9d045a1bb68f28095418c4104df5
Trang <nguyenminhtrang9213@gmail.com>
2025-11-24 21:32:16 +0100

    update

--- src/components/WeatherApp.tsx
+++ src/components/WeatherApp.tsx
@@ -1,13 +1,8 @@
 import { Droplet, MapPin, Search, Wind } from "lucide-react";
-import { useState, type JSX } from "react";
+import { useEffect, useState, type JSX } from "react";
 import type { LucideProps } from "lucide-react";
 import type { ForwardRefExoticComponent } from "react";
-
 import * as Icon from "lucide-react";
-
-// import cloudy from "../assets/images/cloudy.png";
-// import rainy from "../assets/images/rainy.png";
-// import snowy from "../assets/images/snowy.png";
 
 interface Citydata {
   name: string;
@@ -31,13 +26,14 @@ type BackgroundCollection =
 function WeatherApp(): JSX.Element {
   const [cityName, setCityName] = useState<string>("");
   const [weatherData, setWeatherData] = useState<Citydata | null>(null);
+
   const api_key: string = import.meta.env.VITE_OPENAI_API_KEY;
   const iconMap: Record<BackgroundCollection, keyof typeof Icon> = {
     Clear: "Sun",
-    Clouds: "Cloudy",
-    Rain: "CloudHail",
+    Clouds: "Cloud",
+    Rain: "CloudRain",
     Snow: "Snowflake",
-    Haze: "Haze",
+    Haze: "Wind",
     Mist: "CloudFog",
   };
 
@@ -52,18 +48,26 @@ function WeatherApp(): JSX.Element {
     setCityName(event.target.value);
   };
 
-  const getWeatherData = async () => {
-    if (!cityName) return;
+  const handleKeyDown = (
+    event: React.KeyboardEvent<HTMLInputElement>
+  ): void => {
+    if (event.key === "Enter") {
+      getWeatherData(cityName);
+    }
+  };
+
+  const getWeatherData = async (city: string) => {
+    if (!city) return;
 
     try {
       const response = await fetch(
-        `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${api_key}&units=metric`
+        `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${api_key}&units=metric`
       );
 
       const res = await response.json();
       const data = res.list[0];
       const weatherData: Citydata = {
-        name: cityName,
+        name: city,
         main_status: data.weather[0].main,
         temp: Math.floor(data.main.temp),
         humidity: data.main.humidity,
@@ -71,10 +75,18 @@ function WeatherApp(): JSX.Element {
         date: new Date().toDateString(),
       };
       setWeatherData(weatherData);
+      setCityName("");
     } catch (error) {
       console.log(error);
     }
   };
+
+  useEffect(() => {
+    const load = async () => {
+      await getWeatherData("Berlin");
+    };
+    load();
+  }, []);
 
   return (
     <div
@@ -86,35 +98,39 @@ function WeatherApp(): JSX.Element {
         <div className="search ">
           <div className="search-top flex space-x-2 mb-2">
             <MapPin />
-            <div className="capitalize">{cityName}</div>
+            <div className="capitalize">
+              {weatherData?.name ? weatherData?.name : "Berlin"}
+            </div>
           </div>
 
-          <div className="search-bar flex justify-between items-center  border-purple-700  border-2 rounded-full">
+          <div className="search-bar flex justify-between items-center  border-gray-700  border-2 rounded-full">
             <input
               type="text"
               value={cityName}
               onChange={handleCityNameChange}
-              placeholder="Enter location"
+              placeholder="Enter a city name"
               className="flex-1 text-xl outline-none px-4 py-2 placeholder:text-md"
+              onKeyDown={handleKeyDown}
             />
             <Search
-              onClick={getWeatherData}
-              className="cursor-pointer text-purple-700 hover:scale-105 transition duration-150 mr-1"
+              onClick={() => getWeatherData(cityName)}
+              className="cursor-pointer text-gray-700 hover:scale-105 transition duration-150 mr-1"
             />
           </div>
         </div>
         <div className="text-center flex flex-col space-y-4">
-          <div className="h-1/3">
-            <WeatherIcon />
+          <div className="h-1/3 text-center py-6">
+            <WeatherIcon className="mx-auto w-54 h-54" />
           </div>
+
           <div className="text-3xl flex-1">{weatherData?.main_status}</div>
           <div className="text-6xl">{weatherData?.temp}</div>
         </div>
 
-        <div className="text-center mb-4">
+        <div className="text-center my-4">
           <p>{weatherData?.date}</p>
         </div>
-        <div className="flex space-x-2">
+        <div className="flex space-x-2 mt-8">
           <div
             className={`w-1/2 flex flex-col space-y-2 justify-center items-center shadow-2xl opacity-90 ${weatherData?.main_status} rounded-xl p-4`}
           >
